19a5d820a6824ca7baf10ca5eb9b6da48c0a8046
ziadmoftah <ziadh434@gmail.com>
2020-07-13 15:31:08 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define inf INFINITY
+#define file freopen( "input.txt" , "r" , stdin) ; freopen( "output.txt" , "w" , stdout) ;
+#define Gamed ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define _3102_ main()
+#define endl '\n'
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+using namespace std;
+const int N = 2e5 + 100;
+const ll MX = 1e15;
+const ll mod = 1e9 + 7;
+const double eps = 1e-9 ;
+
+
+
+
+
+int _3102_{
+	Gamed
+    
+    return 0;
+}
+ 
